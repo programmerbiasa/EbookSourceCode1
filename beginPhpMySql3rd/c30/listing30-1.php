c6eded3c6226f92454f8113e8697fa6f1b74dda9
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-19 10:14:11 +0700

    Update SQL insert query and parameter binding

--- beginPhpMySql3rd/c30/listing30-1.php
+++ beginPhpMySql3rd/c30/listing30-1.php
@@ -1,16 +1,20 @@
 <?php
+   // chapter 30 Using PHP With MySQL; listing30-1.php  
+
    // Create a new server connection
    $mysqli = new mysqli("127.0.0.1", "catalog_user", "secret", "catalog_prod");
 
    // Create the query and corresponding placeholders
-   $query = "INSERT INTO product SET rowID=NULL, productID=?, 
-             name=?, price=?, description=?";
+   $query = "INSERT INTO product SET id=NULL, sku=?, name=?, price=?, description=?";
+   
+   // Create a statement object
+   $stmt = $mysqli->stmt_init();
 
    // Prepare the statement for execution
    $stmt = $mysqli->prepare($query);
 
    // Bind the parameters
-   $stmt->bind_param('ssds', $productid, $name, $price, $description);
+   $stmt->bind_param('ssds', $sku, $name, $price, $description);
 
    // Assign the posted productid array
    $productidarray = $_POST['productid'];
