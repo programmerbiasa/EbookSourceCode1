733d8f7574f49f006236ffd7c6f367e995b3f905
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-21 14:23:32 +0700

    Create deklarasiVar11.php

---
+++ tipTrikPemrogramanPhp/deklarasiVar11.php
@@ -0,0 +1,9 @@
+ // deklarasi dan inisialisasi variabel 
+ $result = 1 + 5; // valid
+ $RESULT = 2 + 5; // valid beda dg result
+ $_result = 3 + 5; // valid
+ // deklarasi dan inisialisasi variabel
+ $1 = 1 + 5; // tidak valid
+ $1res = 2 + 5; // tidak valid
+ $#res = 3 + 5; // tidak valid
+ $my-res = 4 + 5; // tidak valid
