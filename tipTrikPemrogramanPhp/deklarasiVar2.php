e448ab28b8444be96bf5a6336a3a447d2b9f1636
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-21 15:57:19 +0700

    Add example of dynamic variable usage in PHP
    Demonstrate the use of variable variables in PHP.

--- tipTrikPemrogramanPhp/deklarasiVar2.php
+++ tipTrikPemrogramanPhp/deklarasiVar2.php
@@ -1,6 +1,16 @@
+<?php
 
 $a = "Hello";
+// variable variables
 $$a = "world";
 
 // Memanggil variabel dinamis
 echo "${$a}";
+// ekuivalen dengan berikut
+echo "$Hello";
+// ekuivalen dengan berikut
+echo "{$$a}";
+// Memanggil semua (kedua variabel)
+echo "$a $Hello"; // Mencetak Hello World
+
+?>
