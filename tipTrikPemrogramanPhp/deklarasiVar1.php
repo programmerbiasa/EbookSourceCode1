1ad2040f7056e8a2dca5b0d257c48ec39aebe544
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-22 12:02:31 +0700

    Update deklarasiVar1.php

--- tipTrikPemrogramanPhp/deklarasiVar1.php
+++ tipTrikPemrogramanPhp/deklarasiVar1.php
@@ -1,4 +1,5 @@
- // deklarasi dan inisialisasi variabel 
+<?php
+// deklarasi dan inisialisasi variabel 
  $result = 1 + 5; // valid
  $RESULT = 2 + 5; // valid beda dg result
  $_result = 3 + 5; // valid
@@ -7,3 +8,4 @@
  $1res = 2 + 5; // tidak valid
  $#res = 3 + 5; // tidak valid
  $my-res = 4 + 5; // tidak valid
+?>
